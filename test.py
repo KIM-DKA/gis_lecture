d61c9c0309a17c94a7c1f08931e812dc24f9ffb0
KIM-DKA <rlaemrghk39@gmail.com>
2022-08-24 15:21:55 +0900

    mkdir [image,lecture,packages]

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ import sys
 import numpy as np 
 import pandas as pd 
 import geopandas as gpd
-import imp 
+import importlib
+import openpyxl
+
+sys.path.append('/Applications/anaconda3/envs/myenv3/lib/python3.8/packages')
+
+os.listdir('/Users/dk/Documents/galleria/scheme')
+

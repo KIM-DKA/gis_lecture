a3bb6758432edd9887cfec5d44ae3d1308250933
KIM-DKA <rlaemrghk39@gmail.com>
2022-09-01 16:38:15 +0900

    add file [ulsan.shp]

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+
 import os 
 import sys 
 import numpy as np 
@@ -5,8 +6,31 @@ import pandas as pd
 import geopandas as gpd
 import importlib
 import openpyxl
+from time import time 
+
 
 sys.path.append('/Applications/anaconda3/envs/myenv3/lib/python3.8/packages')
 
-os.listdir('/Users/dk/Documents/galleria/scheme')
+file_path = '/Users/dk/Documents/galleria/scheme/'
+
+start = time() 
+file_nm = list(filter(lambda s : 'xlsx' in s, os.listdir(file_path)))
+end = time() 
+
+file_nm 
+
+file_path + file_nm[0]
+# Excel sheet view 
+
+pd.read_excel(os.listdir)
+
+temp = pd.read_excel(file_path + file_nm[0],sheet_name=None)
+
+help(pd.read_excel)
+
+# count sheet number 
+
+_ = pd.ExcelFile(file_path + file_nm[0])
+
+
 
